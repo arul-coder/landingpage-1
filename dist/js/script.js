ce5771747e89c1f0e4f6e84484cccdb954045019
arul-coder <syayidamrullah@gmail.com>
2024-09-12 08:04:57 +0800

    tinggal edit
    landing page buat jual claude ai pro

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,5 +10,6 @@ const navBar = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
     console.log(window.scrollY);
     const windowPosition = window.scrollY > 0;
-    navBar.classList.toggle('scrolling-active', windowPosition)    
+    navBar.classList.toggle('scrolling-active', windowPosition)   
+    menuNav.classList.remove("menu-active");
 })
